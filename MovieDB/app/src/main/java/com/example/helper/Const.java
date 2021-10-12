d88f15868fa51c369e39586a97436b161a2c449b
Maximus Aurelius Wiranata <maximusaureliusw@gmail.com>
2021-10-12 14:22:53 +0700

    delete

--- MovieDB/app/src/main/java/com/example/helper/Const.java
+++
@@ -1,9 +0,0 @@
-package com.example.helper;
-
-public class Const {
-
-    public static final String API_KEY = "c07fb3d0b0f6bd8484afe39f13c50d3c";
-    public static final String BASE_URL = "https://api.themoviedb.org/3/";
-    public static final String IMG_URL = "https://image.tmdb.org/t/p/w500/";
-
-}
